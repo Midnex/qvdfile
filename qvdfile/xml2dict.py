d1974f7267a88ffc4b6811a8feef2a8af1de38cd
Llane Rost <llanerost@gmail.com>
2021-06-16 17:33:36 -0700

    Formatting

--- qvdfile/xml2dict.py
+++ qvdfile/xml2dict.py
@@ -1,25 +1,25 @@
 """
-
 Converts XML tree to dictionary, see unit tests for usage examples
 
 Simplified to work with QVD XML, do not try to use for general XML to dictionary conversion.
-
 """
 
 import xml.etree.ElementTree as ET
 from collections import defaultdict
 
-def xml2dict( sXml ):
-    """ converts XML string to dictionary """
+
+def xml2dict(sXml):
+    """converts XML string to dictionary"""
 
     if sXml.strip():
         return etree2dict(ET.fromstring(sXml))
     else:
         return dict()
 
-def etree2dict( t ):
-    """ recursive tree converter """
-    
+
+def etree2dict(t):
+    """recursive tree converter"""
+
     d = {t.tag: None}
     children = list(t)
     if children:
@@ -27,8 +27,8 @@ def etree2dict( t ):
         for dc in map(etree2dict, children):
             for k, v in dc.items():
                 dd[k].append(v)
-        d = {t.tag: {k:v[0] if len(v) == 1 else v for k, v in dd.items()}}
+        d = {t.tag: {k: v[0] if len(v) == 1 else v for k, v in dd.items()}}
     if t.text and t.text.strip():
         d[t.tag] = t.text.strip()
-        
+
     return d
